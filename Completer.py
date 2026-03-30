d8c0c40108c4b94ca0779183b6416ed532d830c7
Asif Tanim <tanimasif409@gmail.com>
2026-03-31 02:31:08 +0600

    Update Completer.py
    Refactor Completer to simplify match handling and remove commented-out code.

--- Completer.py
+++ Completer.py
@@ -21,9 +21,9 @@ class Completer:
             new_matches = []
             for m in matches:
                 if os.path.isdir(m):
-                    new_matches.append(m + '/')  # folder হলে / যোগ করো
+                    new_matches.append(m + '/')
                 else:
-                    new_matches.append(m)  # file হলে এমনই রাখো
+                    new_matches.append(m)
             matches = new_matches
         else:
             matches = []
@@ -37,12 +37,12 @@ class Completer:
                     continue
             try:
                 new_matches = []
-                for f in os.listdir('.'):  # current folder এর সব কিছু দেখো
-                    if f.startswith(text):  # text দিয়ে শুরু হলে
-                        if os.path.isdir(f):  # folder হলে
-                            new_matches.append(f + '/')  # শেষে / যোগ করো
+                for f in os.listdir('.'):  
+                    if f.startswith(text): 
+                        if os.path.isdir(f): 
+                            new_matches.append(f + '/') 
                         else:
-                            new_matches.append(f)  # file হলে এমনই রাখো
+                            new_matches.append(f) 
 
                 matches += new_matches
 
@@ -52,48 +52,3 @@ class Completer:
 
 
 
-# class Completer:
-#     def __init__(self):
-#         self.matches = []
-#
-#     def complete(self, text, state):
-#         if state == 0:
-#             self.matches = self.get_matches(text)
-#         try:
-#             return self.matches[state]
-#         except IndexError:
-#             return None
-#
-#     def get_matches(self, txt):
-#         if '/' in txt or txt.startswith('.'):
-#             matches = glob.glob(txt + '*')
-#             new_matches = []
-#             for match in matches:
-#                 if os.path.isdir(match):
-#                     new_matches.append(match + "/")
-#                 else:
-#                     new_matches.append(match)
-#             matches = new_matches
-#             return sorted(matches)
-#         else:
-#             matches = []
-#             sep = ';' if IS_WINDOWS else ':'
-#
-#             for path_dir in os.environ['PATH'].split(sep):
-#                 for f in os.listdir(path_dir):
-#                     if f.startswith(txt):
-#                         matches.append(f)
-#             try:
-#                 new_matches = []
-#                 for f in os.listdir('.'):
-#                     if f.startswith(txt):
-#                         if os.path.isdir(f):
-#                             new_matches.append(f + '/')
-#                         else:
-#                             new_matches.append(f)
-#                     matches += new_matches
-#
-#             except OSError:
-#                 return []
-#
-#             return sorted(set(matches))
